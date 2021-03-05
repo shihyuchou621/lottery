7ac9abe5ffd7b8f6ed2db65fcfd0b8a6ddf1bc7d
shihyuchou <kellyu621@gmail.com>
2021-03-05 20:27:59 +0800

    第一版

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import './App.css';
 import React, { useState } from 'react';
 
-let randomDraw = Math.floor(Math.random() * 1000);
+const QRlist = [
+  {name: "Kelly", QR: 1}, 
+  {name: "Ben", QR: 2}, 
+  {name: "Kon", QR: 3}, 
+  {name: "By", QR: 4}, 
+  {name: "BigCat", QR: 5}, 
+  {name: "SmallCat", QR: 6}
+];
+
 
 export default function App() {
-  const [drawNum, setDrawNum] = useState([]);
+  const [drawList, setDrawList] = useState([]);
+  const [drawNum, setDrawNum] = useState(null);
+  const [awardList, setAwardList] = useState([
+    {award: "first", QR: null}, 
+    {award: "second", QR: null}, 
+    {award: "third", QR: null}]
+  );
 
-  
-  const handleClick = () => {
-    drawNum.push(randomDraw);
+  const draw = () => {
+    let randomDraw = Math.floor(Math.random() * 6);
+    if(drawList.indexOf(randomDraw) > -1) { draw(); }
+    setDrawNum(randomDraw);
   }
 
-  console.log(drawNum, 10);
+  const handleClick = () => {
+    draw();
+    setDrawList([...drawList, QRlist[drawNum].QR]);
+    awardList[0].QR = QRlist[drawNum].QR;
+  }
+  
+  console.log(drawList, 10);
 
+  
   return (
     <div className="App">
       <button onClick={handleClick}>click</button>
-      {drawNum}
+      得獎者：{drawList[drawList.length-1]}
     </div>
   );
 }
